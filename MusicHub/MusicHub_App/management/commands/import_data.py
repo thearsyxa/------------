b63f50c6ebe893924559e46a7dfbfd3d7f8c5408
thearsyxa <geemmy2007@gmail.com>
2025-02-20 19:42:35 +0300

    Добавил requirments.txt

--- MusicHub/MusicHub_App/management/commands/import_data.py
+++ MusicHub/MusicHub_App/management/commands/import_data.py
@@ -8,23 +8,19 @@ class Command(BaseCommand):
     help = 'Импорт данных из Spotify и Википедии'
 
     def handle(self, *args, **kwargs):
-        access_token = 'BQD8aXlX0hfpV-Ljl1_r7guIUDelV0D9305rm5i-ky6CGZFH7jPPCwKbU2lwhbUcWniYad57S9lgIdyUrB6yO5PbTKEN90DdxydYz9xe0207n1KhD6dd8Jps1lxZ-wv87fkVQ0Vju-U'  # Ваш существующий токен доступа
-
-        # Список конкретных исполнителей для импорта
+        access_token = 'BQD8aXlX0hfpV-Ljl1_r7guIUDelV0D9305rm5i-ky6CGZFH7jPPCwKbU2lwhbUcWniYad57S9lgIdyUrB6yO5PbTKEN90DdxydYz9xe0207n1KhD6dd8Jps1lxZ-wv87fkVQ0Vju-U'  
         specific_artists = ["Джизус", "Три дня дождя", "Nirvana", "System of a Down", "Юлия Савичева", "Metallica", "Ваня Дмитриенко", "Michael Bublé", "PHARAOH", "Нервы"]
 
         for artist_name in specific_artists:
             try:
                 self.stdout.write(self.style.SUCCESS(f"Поиск исполнителя: {artist_name}"))
-                # Поиск исполнителя
+                
                 artists = search_artists(artist_name, access_token)
                 for artist_data in artists:
                     try:
                         spotify_artist_id = artist_data['id']
                         self.stdout.write(self.style.SUCCESS(f"Найден исполнитель: {artist_data['name']} (ID: {spotify_artist_id})"))
                         artist_info = get_artist_info(spotify_artist_id, access_token)
-
-                        # Вывод ответа API для отладки
                         self.stdout.write(self.style.SUCCESS(f"Ответ API: {artist_info}"))
 
                         if 'name' in artist_info:
@@ -50,7 +46,6 @@ class Command(BaseCommand):
                         else:
                             self.stdout.write(self.style.WARNING(f'Исполнитель {artist_name} уже существует'))
 
-                        # Импорт альбомов
                         albums_info = get_albums_info(spotify_artist_id, access_token)
                         if not albums_info:
                             self.stdout.write(self.style.ERROR(f"Не удалось получить альбомы для исполнителя: {artist_name}"))
@@ -74,7 +69,6 @@ class Command(BaseCommand):
                             if created:
                                 self.stdout.write(self.style.SUCCESS(f'Добавлен новый альбом: {album.title}'))
 
-                            # Импорт треков
                             tracks_info = get_tracks_info(album_info['id'], access_token)
                             if not tracks_info:
                                 self.stdout.write(self.style.ERROR(f"Не удалось получить треки для альбома: {album_info['name']}"))
@@ -82,7 +76,7 @@ class Command(BaseCommand):
                                 Track.objects.get_or_create(
                                     album=album,
                                     title=track_info['name'],
-                                    duration=timedelta(milliseconds=track_info['duration_ms']),  # Преобразование в timedelta
+                                    duration=timedelta(milliseconds=track_info['duration_ms']), 
                                     defaults={'play_count': track_info.get('play_count')}
                                 )
                     except Exception as e:
